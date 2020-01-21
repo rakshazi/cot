e6ba0fd53928c96a3fd205767a296345077c4c8e
Sargerous <Sarger@bk.ru>
2020-01-21 19:00:02 +0600

    'knopochki'

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source "https://rubygems.org"
 
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo', '~> 1.2'
+gem 'wdm', '~> 0.1.1', :install_if => Gem.win_platform?
+
 gem "jekyll"
+
--- _includes/webp.js
+++ _includes/webp.js
@@ -1,16 +1,1 @@
-(function(window) {
-  var html = document.documentElement,
-    isSupported = null;
 
-  function checkSupport(fn) {
-    var WebP = new Image;
-    WebP.onload = WebP.onerror = function() {
-      isSupported = WebP.height === 2;
-      if (!isSupported) {
-        if (html.className.indexOf("webp") >= 0) html.className = html.className.replace(/\bwebp\b/, "no-webp");
-      }
-    };
-    WebP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"
-  }
-  checkSupport()
-})(window);
